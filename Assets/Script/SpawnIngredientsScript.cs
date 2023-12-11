e2be7733847e7f24c2c0c70f177e6939e02464e5
Luca Dal Maso <lucadalmaso2001@gmail.com>
2023-12-11 14:03:50 +0100

    some implementation

---
+++ Assets/Script/SpawnIngredientsScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnIngredientsScript : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject topBread, bottonBread;
+
+    [SerializeField]
+    GameObject tomato;
+
+    [SerializeField]
+    GameObject insalata;
+
+    [SerializeField]
+    GameObject salame;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
